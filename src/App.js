86abc03cfa1af6f88b0264c298d1b00dd62880bf
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-07-25 02:08:50 +0530

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Complete from './components/Complete'
-import Header from './components/Header/Header'
+import Header from './components/Todo/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Complete/>
     </div>
   );
 }
